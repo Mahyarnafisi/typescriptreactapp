5ddd080e8fcc3ec923d7e2b460eb0bb8738bde42
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-07-08 00:01:42 +0200

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">Welcome to TS</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/"></Route>
+        <Route path="/product"></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
